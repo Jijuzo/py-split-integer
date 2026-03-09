dd6f1e8838e637628d772f4a039a2f521229cd1f
Igor Marczuk <usdreldo@gmail.com>
2026-03-09 19:51:17 +0100

    tests added

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,25 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+    assert split_integer(8, 1) == [8]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    pass
-
-
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    pass
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    pass
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    pass
+def test_should_handle_case_where_value_is_less_than_number_of_parts() -> None:
+    assert split_integer(2, 5) == [0, 0, 0, 1, 1]
+
+
+def test_should_split_correctly_with_large_remainder() -> None:
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+
+
+def test_should_return_list_of_zeros_when_value_is_zero() -> None:
+    assert split_integer(0, 4) == [0, 0, 0, 0]
